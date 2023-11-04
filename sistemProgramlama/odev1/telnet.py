422d9fb467fa14e26df01a630b3d78ddf774c0df
SahinMuhammetAbdullah <m.sahingithub@gmail.com>
2023-11-04 21:59:25 +0300

    main

--- sistemProgramlama/odev1/telnet.py
+++ sistemProgramlama/odev1/telnet.py
@@ -19,43 +19,54 @@ def send_email(username, to, subject, message):
   # Giriş yapmak için kimlik bilgilerini gönder
   client_socket.send(b'EHLO gmail.com\r\n')
   recv_data = client_socket.recv(1024)
+  print('EHLO bağlandı')
   print(recv_data.decode())
 
   client_socket.send(b'AUTH LOGIN\r\n')
   recv_data = client_socket.recv(1024)
+  print('AUTH LOGIN bağlandı')
   print(recv_data.decode())
 
   client_socket.send(b'YXBvNjkwYXBvQGdtYWlsLmNvbQ==\r\n')
   recv_data = client_socket.recv(1024)
+  print('E-posta adresi base64 olarak gönderildi')
   print(recv_data.decode())
 
   client_socket.send(b'c3d2eSBmYW92IHV3ZHQgdmFrYQ==\r\n')
   recv_data = client_socket.recv(1024)
+  print('Şifre base64 olarak gönderildi')
   print(recv_data.decode())
 
   # E-posta gönderme işlemi
   client_socket.send(f'MAIL FROM: <{username}>\r\n'.encode())
   recv_data = client_socket.recv(1024)
+  print('E-posta adresi verildi')
   print(recv_data.decode())
 
   client_socket.send(f'RCPT TO: <{to}>\r\n'.encode())
   recv_data = client_socket.recv(1024)
+  print('Alıcı adresi verildi')
   print(recv_data.decode())
 
   client_socket.send(b'DATA\r\n')
   recv_data = client_socket.recv(1024)
+  print('DATA gönderildi')
   print(recv_data.decode())
 
   client_socket.send(f'Subject: {subject}\r\n\r\n{message}\r\n.\r\n'.encode())
   recv_data = client_socket.recv(1024)
+  print('E-posta gönderildi')
   print(recv_data.decode())
 
   # Bağlantıyı kapat
   client_socket.send(b'QUIT\r\n')
   recv_data = client_socket.recv(1024)
+  print('QUIT gönderildi')
   print(recv_data.decode())
-
+  
   client_socket.close()
+  
+  print('QUIT gönderildi')
 
 
 if __name__ == '__main__':
