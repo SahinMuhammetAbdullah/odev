45d132ef83ede63f95ab0ff86cce65ef02cb35e7
SahinMuhammetAbdullah <m.sahingithub@gmail.com>
2023-11-05 16:46:04 +0300

    main

--- mobilProgramlama/odev4/asynchronous.dart
+++ mobilProgramlama/odev4/asynchronous.dart
@@ -1,46 +1,21 @@
 import 'dart:async';
-import 'dart:io';
 
 Future<void> main() async {
-  List<int> inputList = await readIntegersFromInput();
+  final List<int> inputList = [1, 2, 3, 4, 5];
+  final List<int> modifiedList = await multiplyAsync(inputList);
 
-  if (inputList.isEmpty) {
-    print("The list is empty.");
-    return;
-  }
-
-  List<int> modifiedList = await multiplyByTwoAsync(inputList);
-
-  print("Modified List: $modifiedList");
+  print('Original List: $inputList');
+  print('Modified List: $modifiedList');
 }
 
-Future<List<int>> readIntegersFromInput() async {
-  print("Enter a list of integers separated by spaces:");
-  String? input = stdin.readLineSync();
-
-  List<int> inputList = input!.split(' ').map((String str) {
-    try {
-      return int.parse(str);
-    } catch (e) {
-      return 0; // Use 0 for non-integer inputs
-    }
+Future<List<int>> multiplyAsync(List<int> inputList) async {
+  final List<Future<int>> futures = inputList.map((int value) async {
+    await Future.delayed(
+        Duration(seconds: 1));
+    return value * 2;
   }).toList();
 
-  return inputList;
-}
-
-Future<List<int>> multiplyByTwoAsync(List<int> inputList) async {
-  List<int> modifiedList = [];
-
-  for (int number in inputList) {
-    int result = await multiplyByTwo(number);
-    modifiedList.add(result);
-  }
+  final List<int> modifiedList = await Future.wait(futures);
 
   return modifiedList;
-}
-
-Future<int> multiplyByTwo(int number) async {
-  await Future.delayed(Duration(seconds: 1)); // Simulate asynchronous operation
-  return number * 2;
 }
